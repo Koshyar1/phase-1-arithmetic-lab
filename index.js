5ab5f9eb0a6a73f3570ecdf8eb747350d75b5b8c
koshyar <ensw03420@student.su.edu.krd>
2024-03-13 12:13:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.ceil(Math.random());
+
+let num3 = 11;
+let num4 = 7;
+let mod = num3 % num4;
+
+let set = [1, 4, 6, 9, 20, 15, 16];
+let max = Math.max(...set);
